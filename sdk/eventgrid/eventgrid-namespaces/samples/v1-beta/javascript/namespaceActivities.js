1adfc19d6e9afebb68765db10ab8cc82370df5b1
Tiancheng Zhang (WICRESOFT NORTH AMERICA LTD) <v-zhangtianc@microsoft.com>
2024-07-18 11:09:59 +0800

    :sparkles: All Samples Run Successfully!

--- sdk/eventgrid/eventgrid-namespaces/samples/v1-beta/javascript/namespaceActivities.js
+++ sdk/eventgrid/eventgrid-namespaces/samples/v1-beta/javascript/namespaceActivities.js
@@ -5,7 +5,7 @@
  * @summary Publish and Receive events to Event Grid.
  */
 
-const { EventGridClient } = require("@azure/eventgrid-namespaces");
+const { EventGridSenderClient } = require("@azure/eventgrid-namespaces");
 const { AzureKeyCredential } = require("@azure/core-auth");
 
 const dotenv = require("dotenv");
@@ -17,29 +17,24 @@ const endpoint = process.env["EVENT_GRID_NAMESPACES_ENDPOINT"] ?? "https://endpo
 const key = process.env["EVENT_GRID_NAMESPACES_KEY"] ?? "api_key";
 const eventSubscripionName = process.env["EVENT_SUBSCRIPTION_NAME"] ?? "testsubscription1";
 const topicName = process.env["TOPIC_NAME"] ?? "testtopic1";
-
 async function main() {
-  // Create the client used to publish events
-  const client = new EventGridClient(endpoint, new AzureKeyCredential(key));
-
-  // publishes a single cloud event
-  const eventId = `singleEventIdV210001`;
-  const cloudEvent = {
-    type: "example",
-    source: "https://example.com",
-    id: eventId,
-    time: new Date(),
-    data: {
-      resourceUri: "https://dummyurl.com",
-    },
-    specversion: "1.0",
-  };
-  // Publish the Cloud Event
-  await client.publishCloudEvent(cloudEvent, topicName);
-  // Receive the Published Cloud Event
-  const receiveResult = await client.receiveCloudEvents(topicName, eventSubscripionName);
-  // The Received Cloud Event ID must be equal to the ID of the Event that was published.
-  console.log(`Received Event ID: ${receiveResult.value[0].event.id}`);
+  const client = new EventGridSenderClient(
+  endpoint,
+  new AzureKeyCredential(key),
+  topicName
+);
+const cloudEvent = {
+  type: "example",
+  source: "https://example.com",
+  id: `singleEventIdV210001`,
+  time: new Date(),
+  data: {
+    resourceUri: "https://dummyurl.com",
+  },
+  specversion: "1.0",
+};
+// Publish the Cloud Event
+await client.sendEvents(cloudEvent);
 }
 
 main().catch((err) => {
